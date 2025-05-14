436a06e98c47d9cb3d46612736ecc978755db128
Jordan Chalupka <9794216+jordan-chalupka@users.noreply.github.com>
2025-05-14 14:30:14 -0400

    InsurOS: Rename references to Twenty

--- packages/twenty-front/src/testing/mock-data/users.ts
+++ packages/twenty-front/src/testing/mock-data/users.ts
@@ -43,8 +43,8 @@ export const workspaceLogoUrl =
 export const mockCurrentWorkspace: Workspace = {
   subdomain: 'acme.twenty.com',
   id: '7dfbc3f7-6e5e-4128-957e-8d86808cdf6w',
-  displayName: 'Twenty',
-  inviteHash: 'twenty.com-invite-hash',
+  displayName: 'InsurOS',
+  inviteHash: 'insuros.ca-invite-hash',
   logo: workspaceLogoUrl,
   isPublicInviteLinkEnabled: true,
   allowImpersonation: true,
@@ -55,7 +55,7 @@ export const mockCurrentWorkspace: Workspace = {
   isCustomDomainEnabled: false,
   workspaceUrls: {
     customUrl: undefined,
-    subdomainUrl: 'twenty.twenty.com',
+    subdomainUrl: 'insuros.insuros.ca',
   },
   isMicrosoftAuthEnabled: false,
   featureFlags: [
