d215c87b0d48a0bad28fee6d14a6f7b3f4a2c50d
Jordan Chalupka <9794216+jordan-chalupka@users.noreply.github.com>
2025-05-14 14:30:14 -0400

    InsurOS: Rename references to Twenty

--- packages/twenty-server/src/engine/core-modules/twenty-config/config-variables.ts
+++ packages/twenty-server/src/engine/core-modules/twenty-config/config-variables.ts
@@ -294,7 +294,7 @@ export class ConfigVariables {
     description: 'Name used in the From header for outgoing emails',
     type: ConfigVariableType.STRING,
   })
-  EMAIL_FROM_NAME = 'Felix from Twenty';
+  EMAIL_FROM_NAME = 'Jordan from InsurOS';
 
   @ConfigVariablesMetadata({
     group: ConfigVariablesGroup.EmailSettings,
@@ -1118,7 +1118,7 @@ export class ConfigVariables {
 
   @ConfigVariablesMetadata({
     group: ConfigVariablesGroup.ServerConfig,
-    description: 'Twenty server version',
+    description: 'InsurOS server version',
     type: ConfigVariableType.STRING,
     isEnvOnly: true,
   })
