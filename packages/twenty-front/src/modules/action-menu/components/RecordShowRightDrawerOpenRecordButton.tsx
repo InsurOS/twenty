7d262ec1c51025e35259a4e41cfa98105a1fcb3e
Jordan Chalupka <9794216+jordan-chalupka@users.noreply.github.com>
2025-05-16 11:11:58 -0400

    InsurOS: Custom colour scheme

--- packages/twenty-front/src/modules/action-menu/components/RecordShowRightDrawerOpenRecordButton.tsx
+++ packages/twenty-front/src/modules/action-menu/components/RecordShowRightDrawerOpenRecordButton.tsx
@@ -133,7 +133,7 @@ export const RecordShowRightDrawerOpenRecordButton = ({
     <Button
       title="Open"
       variant="primary"
-      accent="blue"
+      accent="green"
       size="medium"
       Icon={IconBrowserMaximize}
       hotkeys={[getOsControlSymbol(), '⏎']}
