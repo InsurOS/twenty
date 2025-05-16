c166bbf469735a57d88a0b4e5169b961700befca
Jordan Chalupka <9794216+jordan-chalupka@users.noreply.github.com>
2025-05-16 11:11:58 -0400

    InsurOS: Custom colour scheme

--- packages/twenty-front/src/modules/settings/serverless-functions/components/tabs/SettingsServerlessFunctionCodeEditorTab.tsx
+++ packages/twenty-front/src/modules/settings/serverless-functions/components/tabs/SettingsServerlessFunctionCodeEditorTab.tsx
@@ -1,6 +1,6 @@
 import {
-  File,
-  SettingsServerlessFunctionCodeEditor,
+    File,
+    SettingsServerlessFunctionCodeEditor,
 } from '@/settings/serverless-functions/components/SettingsServerlessFunctionCodeEditor';
 import { SETTINGS_SERVERLESS_FUNCTION_TAB_LIST_COMPONENT_ID } from '@/settings/serverless-functions/constants/SettingsServerlessFunctionTabListComponentId';
 import { SettingsServerlessFunctionHotkeyScope } from '@/settings/serverless-functions/types/SettingsServerlessFunctionHotKeyScope';
@@ -15,10 +15,10 @@ import { useHotkeyScopeOnMount } from '~/hooks/useHotkeyScopeOnMount';
 import { useNavigateSettings } from '~/hooks/useNavigateSettings';
 import { Button, CoreEditorHeader } from 'twenty-ui/input';
 import {
-  H2Title,
-  IconGitCommit,
-  IconPlayerPlay,
-  IconRestore,
+    H2Title,
+    IconGitCommit,
+    IconPlayerPlay,
+    IconRestore,
 } from 'twenty-ui/display';
 import { Section } from 'twenty-ui/layout';
 
