12978e9be8569a8d682191b58274788f6c8cb514
Ronnie Feng <ronnie@ronniefeng.com>
2025-06-03 18:20:01 -0400

    linting stuff

--- packages/twenty-front/src/modules/settings/integrations/utils/AdminAccounts.ts
+++ packages/twenty-front/src/modules/settings/integrations/utils/AdminAccounts.ts
@@ -1,3 +1,4 @@
 export const ADMIN_ACCOUNTS = ['jordan@insuros.ca', 'ronnie@insuros.ca'];
 
-export const isInsurOSAdminAccount = (email: string) => ADMIN_ACCOUNTS.includes(email);
+export const isInsurOSAdminAccount = (email: string) =>
+  ADMIN_ACCOUNTS.includes(email);
