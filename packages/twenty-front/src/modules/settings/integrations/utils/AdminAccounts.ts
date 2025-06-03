e9061c3d529e8d48a35ac2bc0f8dccb811244846
Ronnie Feng <ronnie@ronniefeng.com>
2025-06-03 18:40:38 -0400

    we should add to lowercase just to be safe

--- packages/twenty-front/src/modules/settings/integrations/utils/AdminAccounts.ts
+++ packages/twenty-front/src/modules/settings/integrations/utils/AdminAccounts.ts
@@ -1,4 +1,4 @@
 export const ADMIN_ACCOUNTS = ['jordan@insuros.ca', 'ronnie@insuros.ca'];
 
 export const isInsurOSAdminAccount = (email: string) =>
-  ADMIN_ACCOUNTS.includes(email);
+  ADMIN_ACCOUNTS.includes(email.toLowerCase());
