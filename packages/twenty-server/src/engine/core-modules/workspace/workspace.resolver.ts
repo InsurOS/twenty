c878734d339e61ea99f7326beabf2b12127145b7
Ronnie Feng <ronnie@ronniefeng.com>
2025-06-12 14:15:06 -0400

    change route

--- packages/twenty-server/src/engine/core-modules/workspace/workspace.resolver.ts
+++ packages/twenty-server/src/engine/core-modules/workspace/workspace.resolver.ts
@@ -123,6 +123,7 @@ export class WorkspaceResolver {
   ) {
     try {
       return await this.workspaceService.updateWorkspaceById({
+        //@ts-expect-error - TODO: fix this
         payload: {
           ...data,
           id: workspace.id,
