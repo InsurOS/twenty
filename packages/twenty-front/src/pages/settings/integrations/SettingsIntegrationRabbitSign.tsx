601e776b8a5dc0c9d298946407166ff87e74017e
Ronnie Feng <ronnie@ronniefeng.com>
2025-06-12 14:28:57 -0400

    getting the routes to work properly

---
+++ packages/twenty-front/src/pages/settings/integrations/SettingsIntegrationRabbitSign.tsx
@@ -0,0 +1,48 @@
+import { SettingsPageContainer } from '@/settings/components/SettingsPageContainer';
+import { SettingsIntegrationPreview } from '@/settings/integrations/components/SettingsIntegrationPreview';
+import { useSettingsIntegrationCategories } from '@/settings/integrations/hooks/useSettingsIntegrationCategories';
+import { SettingsPath } from '@/types/SettingsPath';
+import { SubMenuTopBarContainer } from '@/ui/layout/page/components/SubMenuTopBarContainer';
+import { H2Title } from 'twenty-ui/display';
+import { Section } from 'twenty-ui/layout';
+import { getSettingsPath } from '~/utils/navigation/getSettingsPath';
+
+export const SettingsIntegrationRabbitSign = () => {
+  const [integrationCategoryAll] = useSettingsIntegrationCategories();
+  const integration = integrationCategoryAll.integrations.find(
+    ({ from: { key } }) => key === 'rabbit-sign',
+  );
+
+  if (!integration) {
+    return null;
+  }
+
+  return (
+    <SubMenuTopBarContainer
+      title={integration.text}
+      links={[
+        {
+          children: 'Workspace',
+          href: getSettingsPath(SettingsPath.Workspace),
+        },
+        {
+          children: 'Integrations',
+          href: getSettingsPath(SettingsPath.Integrations),
+        },
+        { children: integration.text },
+      ]}
+    >
+      <SettingsPageContainer>
+        <SettingsIntegrationPreview
+          integrationLogoUrl={integration.from.image}
+        />
+        <Section>
+          <H2Title
+            title={`${integration.text} database`}
+            description={`Connect or access your ${integration.text} data`}
+          />
+        </Section>
+      </SettingsPageContainer>
+    </SubMenuTopBarContainer>
+  );
+};
