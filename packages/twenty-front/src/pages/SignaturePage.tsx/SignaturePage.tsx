3b6087f26570189cd204e32bb4c45c0cc51280fa
Ronnie Feng <ronnie@ronniefeng.com>
2025-06-15 14:37:23 -0400

    initial structure

--- packages/twenty-front/src/pages/SignaturePage.tsx/SignaturePage.tsx
+++ packages/twenty-front/src/pages/SignaturePage.tsx/SignaturePage.tsx
@@ -1,6 +1,24 @@
 import { useParams } from 'react-router-dom';
 
+import { PageHeaderToggleCommandMenuButton } from '@/ui/layout/page-header/components/PageHeaderToggleCommandMenuButton';
+import { PageBody } from '@/ui/layout/page/components/PageBody';
+import { PageContainer } from '@/ui/layout/page/components/PageContainer';
+import { PageHeader } from '@/ui/layout/page/components/PageHeader';
+import { PageTitle } from '@/ui/utilities/page-title/components/PageTitle';
+
 export const SignaturePage = () => {
   const { signatureId } = useParams();
-  return <div>SignaturePage {signatureId}</div>;
+
+  return (
+    <PageContainer>
+      <PageTitle title="Create Signature" />
+      <PageHeader title="Create Signature">
+        <PageHeaderToggleCommandMenuButton />
+      </PageHeader>
+      <PageBody>
+        {/* Add your main content here */}
+        <div style={{ padding: 20 }}>Signature ID: {signatureId}</div>
+      </PageBody>
+    </PageContainer>
+  );
 };
