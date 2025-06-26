c9bc1fde8d081349250aafb3ce7b5778b06e68f4
Jordan Chalupka <jordanchalupka@gmail.com>
2025-06-26 15:40:26 -0400

    update payload structure

--- packages/twenty-server/src/modules/rabbitsign/dtos/rabbit-sign-webhook-payload.dto.ts
+++ packages/twenty-server/src/modules/rabbitsign/dtos/rabbit-sign-webhook-payload.dto.ts
@@ -1,55 +1,12 @@
-import { Type } from 'class-transformer';
-import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
-
-export class RabbitSignSignerDto {
-  @IsString()
-  email: string;
-
-  @IsString()
-  name: string;
-
-  @IsString()
-  status: string;
-
-  @IsString()
-  signingOrder: number;
-}
+import { IsString } from 'class-validator';
 
 export class RabbitSignWebhookPayloadDto {
   @IsString()
   folderId: string;
 
   @IsString()
-  creatorEmail: string;
+  eventName: string;
 
   @IsString()
-  title: string;
-
-  @IsString()
-  summary: string;
-
-  @IsString()
-  folderStatus: string;
-
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => RabbitSignSignerDto)
-  signers: RabbitSignSignerDto[];
-
-  @IsArray()
-  ccList: any[];
-
-  @IsString()
-  creationTimeUtc: string;
-
-  @IsString()
-  downloadUrl: string;
-
-  @IsOptional()
-  @IsString()
-  workspaceId?: string;
-
-  @IsOptional()
-  @IsString()
-  signatureId?: string;
+  signerEmail: string;
 } 
