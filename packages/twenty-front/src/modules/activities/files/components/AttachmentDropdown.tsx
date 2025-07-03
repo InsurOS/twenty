0a3231a7ed69792fba3878670bdf1231143bd56f
Ronnie Feng <ronnie@ronniefeng.com>
2025-07-02 21:38:40 -0400

    dynamic width

--- packages/twenty-front/src/modules/activities/files/components/AttachmentDropdown.tsx
+++ packages/twenty-front/src/modules/activities/files/components/AttachmentDropdown.tsx
@@ -1,9 +1,9 @@
 import { AttachmentSignatureAuditTrailMenuItem } from '@/activities/files/components/AttachmentSignatureAuditTrailMenuItem';
 import { AttachmentSignatureSignedAttachmentMenuItem } from '@/activities/files/components/AttachmentSignatureSignedAttachmentMenuItem';
+import { useGetMenuDropdownWidth } from '@/activities/files/hooks/useGetMenuDropdownWidth';
 import { Dropdown } from '@/ui/layout/dropdown/components/Dropdown';
 import { DropdownContent } from '@/ui/layout/dropdown/components/DropdownContent';
 import { DropdownMenuItemsContainer } from '@/ui/layout/dropdown/components/DropdownMenuItemsContainer';
-import { GenericDropdownContentWidth } from '@/ui/layout/dropdown/constants/GenericDropdownContentWidth';
 import { useDropdown } from '@/ui/layout/dropdown/hooks/useDropdown';
 import {
   IconDotsVertical,
@@ -32,6 +32,7 @@ export const AttachmentDropdown = ({
   onSignature,
   signatureId,
 }: AttachmentDropdownProps) => {
+  const dropdownWidth = useGetMenuDropdownWidth({ signatureId });
   const dropdownId = `${scopeKey}-settings-field-active-action-dropdown`;
 
   const { closeDropdown } = useDropdown(dropdownId);
@@ -63,7 +64,7 @@ export const AttachmentDropdown = ({
         <LightIconButton Icon={IconDotsVertical} accent="tertiary" />
       }
       dropdownComponents={
-        <DropdownContent widthInPixels={GenericDropdownContentWidth.Medium}>
+        <DropdownContent widthInPixels={dropdownWidth}>
           <DropdownMenuItemsContainer scrollable={false}>
             {Boolean(onSignature) && (
               <MenuItem
